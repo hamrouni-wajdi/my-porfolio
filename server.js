7e9a79f8910a75df780fc39a7b1d26493a908254
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2022-08-20 15:52:51 +0100

    request logged in the backend

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static("./"));
 
+app.post("/form", (req, res) => {
+  console.log(req.params, req.query, req.body);
+});
 app.listen(3000, () => {
   console.log(`listening on ${port}`);
 });
