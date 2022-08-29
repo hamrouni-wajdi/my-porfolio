a7570b72b2bb4342635724a70233f2f1e1cd3a5c
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2022-08-29 20:43:59 +0100

    trying to fix node mailer

--- middelwares/mail.js
+++ middelwares/mail.js
@@ -1,6 +1,5 @@
-require('dotenv').config()
+require("dotenv").config();
 const nodemailer = require("nodemailer");
-
 
 var transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -9,30 +8,34 @@ var transporter = nodemailer.createTransport({
   port: 465,
 
   auth: {
+    type: "login",
     user: process.env.User,
     pass: process.env.Password,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  logger: true,
 });
 
-module.exports = function emailMiddelware(req,res,next){
+module.exports = function emailMiddelware(req, res, next) {
   var mailOptions = {
     from: req.body.contactEmail,
     to: process.env.Reciever,
     subject: req.body.contactSubject + " from " + req.body.contactName,
     text: req.body.contactMessage,
   };
-  
+
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
     } else {
       console.log("Email sent: " + info.response);
-      res.status(200).send('OK')
+      res.status(200).send("OK");
     }
-    
-    next()
-  });
-  
-}
 
-module.exports
+    next();
+  });
+};
+
+module.exports;
