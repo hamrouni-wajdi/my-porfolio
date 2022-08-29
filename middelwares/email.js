ce48073b77dd97805ab7ce9e5998329ea8a6d8af
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2022-08-29 21:43:44 +0100

    test in prod

--- middelwares/email.js
+++ middelwares/email.js
@@ -1,9 +1,11 @@
 //require("dotenv").config();
+import dotenv from 'dotenv'
+dotenv.config()
 import { SMTPClient } from "emailjs";
 
 var server = new SMTPClient({
-  user: "devuptunisia@gmail.com",
-  password: "sbwxbyvtzvoxwzip",
+  user: process.env.User,
+  password: process.env.Password,
   host: "smtp.gmail.com",
   ssl: true,
 });
@@ -21,26 +23,25 @@ var server = new SMTPClient({
 //   }
 // );
 
-module.exports = function emailMiddelware2(req, res, next) {
-    var mailOptions = {
-      from: req.body.contactEmail,
-      to: process.env.Reciever,
-      subject: req.body.contactSubject + " from " + req.body.contactName,
-      text: req.body.contactMessage,
-    };
+export default function emailMiddelware2(req, res, next) {
+  var mailOptions = {
+    from: req.body.contactEmail,
+    to: process.env.Reciever,
+    subject: req.body.contactSubject + " from " + req.body.contactName,
+    text: req.body.contactMessage,
+  };
 
   server.send(
     {
-      text: 'req.body.contactMessage',
-      from: "wajdihamrouni501@gmail.com",
-      to: "devuptunisia@gmail.com",
+      text: req.body.contactMessage + " " + req.body.contactEmail,
+      from: req.body.contactEmail,
+      to: "hamrouniwajdi14@gmail.com",
       cc: "",
-      subject: 'req.body.contactSubject' + " from " + 'req.body.contactName',
+      subject: req.body.contactSubject + " from " + req.body.contactName,
     },
     function (err, message) {
       console.log(err || message);
     }
   );
   next();
-};
-
+}
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-require('dotenv').config()
-const express = require("express");
+import dotenv from 'dotenv'
+dotenv.config()
+import  express  from "express";
 const app = express();
-const emailMiddelware = require("./middelwares/mail")
+//const emailMiddelware = require("./middelwares/mail")
 const port = process.env.PORT || 3000;
-const emailMiddelware2 = require("./middelwares/email")
+//const emailMiddelware2 = require("./middelwares/email")
+
+import emailMiddelware2 from "./middelwares/email.js";
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static("./"));
 //app.use(emailMiddelware)
 
-
-app.post("/form",emailMiddelware2);
+app.post("/form", emailMiddelware2);
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
